d669c187a61a33057597b968fc944948d577abfd
ppfeufer <ppfeufer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-09 10:13:43 +0000

    ready for wp 3.3
    git-svn-id: https://plugins.svn.wordpress.org/facebook-fanbox-with-css-support/trunk@460899 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook-fanbox-with-css-support.php
+++ facebook-fanbox-with-css-support.php
@@ -3,48 +3,16 @@
  * Plugin Name: Facebook Fanbox (with CSS Support)
  * Plugin URI: http://blog.ppfeufer.de/wordpress-plugin-facebook-fanbox-with-css-support/
  * Description: Add a sidebarwidget with a fully css-customisable facebook fanbox to your WordPress-Blog.
- * Version: 1.2.0
+ * Version: 1.2.1
  * Author: H.-Peter Pfeufer
  * Author URI: http://ppfeufer.de
- */
-
-/**
- * Changelog
- * = 1.2.0 (16.03.2011) =
- * Fix: Check if all FB-Scripts are loaded correctly.
- *
- * = 1.1.5 (16.03.2011) =
- * Fix: Typo in function call.
- *
- * = 1.1.4 (09.03.2011) =
- * Fix: Corrected javascript-call. Type was missing.
- *
- * = 1.1.3 (09.02.2011) =
- * Fix: hide titlebar if its empty.
- *
- * = 1.1.2 (26.01.2011) =
- * Fix: corrected a name of an internal function call. It was not affecting any service but it was just a confusing name :-)
- *
- * = 1.1.1 (18.01.2011) =
- * Fix: fixed errormessage on firtst activation (thanks to <a href="http://bloggonaut.net/">Jonas</a> for reporting).
- *
- * = 1.1.0 (17.01.2011) =
- * Fix: Moved CSS to upload-dir, so its not effected on upadtes. **Please make sure to control and save your settings after this update**
- * Update: German translation
- *
- * = 1.0.1 (12.01.2011) =
- * Fix: Setting the current locale (<em>must be defined in wp-config.php</em>)
- *
- * = 1.0.0 (11.01.2011) =
- * Initial Release.
- * Added widget-settings.
  */
 
 if(!defined('PPFEUFER_FLATTRSCRIPT')) {
 	define('PPFEUFER_FLATTRSCRIPT', 'http://cdn.ppfeufer.de/js/flattr/flattr.js');
 }
 
-define('FACEBOOK_FANBOX_WITH_CSS_VERSION', '1.2.0');
+define('FACEBOOK_FANBOX_WITH_CSS_VERSION', '1.2.1');
 
 define('FANBOX_CSS_FILE_DEFAULT', WP_PLUGIN_DIR . '/' . str_replace(basename( __FILE__), "", plugin_basename(__FILE__)) . 'css/facebook-fanbox.css');
 define('FANBOX_CSS_FILE', WP_CONTENT_DIR . '/uploads/facebook-fanbox.css');
